bea04c9d4e4be65807ada4aab833b48928db37be
allaspandana <24b01a1205@svecw.edu.in>
2026-07-21 22:58:04 +0530

    Authentication system completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,110 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import RegisterForm, LoginForm
+
+def register_view(request):
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            form.save()
+
+            messages.success(request, "Registration Successful!")
+
+            return redirect("login")
+
+        else:
+
+            messages.error(request, "Please correct the errors below.")
+
+    else:
+
+        form = RegisterForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form
+        }
+    )
+
+
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            user = form.get_user()
+
+            login(request, user)
+
+            messages.success(
+                request,
+                "Welcome Back!"
+            )
+
+            if user.role == "Admin":
+                return redirect("dashboard")
+
+            elif user.role == "Team Leader":
+                return redirect("dashboard")
+
+            elif user.role == "Developer":
+                return redirect("dashboard")
+
+            elif user.role == "Tester":
+                return redirect("dashboard")
+
+            else:
+                return redirect("dashboard")
+
+        else:
+
+            messages.error(
+                request,
+                "Invalid Username or Password."
+            )
+
+    else:
+
+        form = LoginForm()
+
+    return render(
+        request,
+        "registration/login.html",
+        {
+            "form": form
+        }
+    )
+
+
+def logout_view(request):
+
+    logout(request)
+
+    messages.success(
+        request,
+        "Logged Out Successfully."
+    )
+
+    return redirect("login")
+
+
+@login_required(login_url="login")
+def dashboard(request):
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {"user": request.user}
+    )
--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,8 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 AUTH_USER_MODEL = 'accounts.CustomUser'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
